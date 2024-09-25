41d5c247bcfbd18070b3e89267f7b0c0fd1ce924
Work-Alexey <inactive0073@gmail.com>
2024-09-25 11:59:17 +0400

    Update people.py, swagger.yml; added PUT, GET, CREATE methods like one person and getting all person;

--- people.py
+++ people.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from flask import abort, make_response
 
 def get_timestamp():
     return datetime.now().strftime(('%Y-%m-%d %H:%M:%S'))
@@ -21,5 +22,60 @@ PEOPLE = {
     }
 }
 
-def read_all():
+def read_all(): # Отобразить данные всех пользователей
     return list(PEOPLE.values())
+
+def read_one(lname:str):
+    if lname in PEOPLE:
+        print(PEOPLE[lname])
+        return PEOPLE[lname]
+    else:
+        abort(
+            404, f"Person with last name {lname} not found"
+        )
+
+# def read_one(lname:str):
+#     print(PEOPLE.get(lname))
+#     return PEOPLE.get(lname, 
+#                       abort(
+#             404, f"Person with last name {lname} not found"
+#             ))
+
+def create(person:dict): # Добавить нового пользователя
+    lname = person.get('lname')
+    fname = person.get('fname', '')
+    
+    if lname and lname not in PEOPLE:
+        PEOPLE[lname] = {
+            'lname': lname,
+            'fname': fname,
+            'timestamp': get_timestamp()
+        }
+        return PEOPLE[lname], 201
+    else:
+        abort(
+            406,
+            f"Person with last name {lname} already exists"
+        )
+        
+def update(lname, person):
+    if lname in PEOPLE:
+        PEOPLE[lname]['fname'] = person.get('fname', PEOPLE[lname]['fname'])
+        PEOPLE[lname]['timestamp'] = get_timestamp()
+    else:
+        abort(
+            404,
+            f"Person with last name {lname} not found"
+            )
+        
+def delete(lname):
+    if lname in PEOPLE:
+        del PEOPLE[lname]
+        return make_response(
+            f"{lname} successfully deleted", 200
+        )
+    else:
+        abort(
+            404,
+            f"Person with last name {lname} not found"
+        )
