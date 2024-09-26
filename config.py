4bea18868daeb5a5ee93cc9679e2bbe6ef74c865
Work-Alexey <inactive0073@gmail.com>
2024-09-26 13:54:21 +0400

    Replace version python from 3.12 to 3.10, finished 2 part and added connection through SQLAlchemy

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 # config.py
-from pathlib import Path
-from connexion import App
-from flask_sqlalchemy import SQLAlchemy
-from flask_marshmallow import Marshmallow
+import pathlib
 
-basedir = Path(__file__).parent.resolve()
-connex_app = App(__name__, specification_dir=basedir)
+import connexion
+from flask_marshmallow import Marshmallow
+from flask_sqlalchemy import SQLAlchemy
+
+basedir = pathlib.Path(__file__).parent.resolve()
+connex_app = connexion.App(__name__, specification_dir=basedir)
 
 app = connex_app.app
 app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{basedir / 'people.db'}"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
